9947d97d83e3f125d36ae3cb1d153c0abb53436d
MytsV <mytsvictoria@gmail.com>
2024-11-04 17:35:05 +0200

    Add navigation links to the header

--- src/app/_components/layouts/page-layout.tsx
+++ src/app/_components/layouts/page-layout.tsx
@@ -1,12 +1,19 @@
 "use client"
 import { Header } from "@maany_shr/rage-ui-kit"
 import { SiteFooter } from "@maany_shr/rage-ui-kit"
+import Link from "next/link";
 
 export const PageLayout = (props: {children: React.ReactNode}) =>{
+    const headerLinks = [
+        <Link key="research_contexts_link" href="/">Research Contexts</Link>,
+        <Link key="sources_link" href="/sources">Sources</Link>,
+        <a key="docs_link" href="https://dream-aim-deliver.github.io/planckster-docs/" target="_blank">Documentation</a>
+    ];
+
     return (
         <div className="min-h-screen flex flex-col box-shadow shadow-lg bg-neutral-50 dark:bg-neutral-900">
             {/* Header */}
-            <Header />
+            <Header>{headerLinks}</Header>
 
             {/* Main content */}
             <main className="flex flex-grow container mx-auto p-4">
