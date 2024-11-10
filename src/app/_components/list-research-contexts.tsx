bdf5be29bbbb85628ebfca32c39adb3ea7ea2b3a
maany <imptodefeat@gmail.com>
2024-11-10 21:04:35 +0100

    reset view model to request after creating research context errors happen

--- src/app/_components/list-research-contexts.tsx
+++ src/app/_components/list-research-contexts.tsx
@@ -14,6 +14,7 @@ export function ListResearchContextsClientPage(props: { viewModel: TListResearch
   const [listResearchContextsViewModel, setListResearchContextsViewModel] = useState<TListResearchContextsViewModel>(props.viewModel);
   const [createResearchContextViewModel, setCreateResearchContextViewModel] = useState<TCreateResearchContextViewModel>({
     status: "request",
+    researchContextName: "",
   } as TCreateResearchContextViewModel);
 
   const router = useRouter();
