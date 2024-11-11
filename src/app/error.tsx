acc05c732e97c770a0d22bc579ba0944c4117844
MytsV <mytsvictoria@gmail.com>
2024-11-11 17:03:28 +0200

    Fix error page layout and functionality

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,31 +1,24 @@
-'use client' // Error boundaries must be Client Components
- 
-import { useEffect } from 'react'
-import {ErrorPage } from "@maany_shr/rage-ui-kit"
+"use client"; // Error boundaries must be Client Components
 
-export default function Error(props: {
-  error: Error & { digest?: string }
-  reset: () => void
-}) {
+import { useEffect } from "react";
+import { ErrorPage } from "@maany_shr/rage-ui-kit";
+
+export default function Error(props: { error: Error & { digest?: string }; reset: () => void }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(props.error)
-  }, [props.error])
- 
+    console.error(props.error);
+  }, [props.error]);
+
   return (
-    <div>
-      <ErrorPage error={{
+    <ErrorPage
+      error={{
         message: props.error.message,
-        digest: props.error.digest
-      }}/>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => props.reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  )
+        digest: props.error.digest,
+      }}
+      reset={{
+        action: () => window.location.reload(),
+        message: "Try again",
+      }}
+    />
+  );
 }
