c2127675ac2a4f2192be9b46932fda79bf07dd60
maany <imptodefeat@gmail.com>
2024-11-15 18:52:48 +0100

    config: add NEXT_PUBLIC_APP_URL for trpc calls behind proxies

--- src/lib/infrastructure/client/trpc/vanilla-api.ts
+++ src/lib/infrastructure/client/trpc/vanilla-api.ts
@@ -1,12 +1,12 @@
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import superjson from 'superjson';
 import type { AppRouter } from '~/lib/infrastructure/server/trpc/app-router';
-
+import env from '../config/env';
 // TODO: add client side env variable to configure the url of the trpc server
 const client = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-        url: 'http://localhost:3000/api/trpc',
+        url: `${env.NEXT_PUBLIC_APP_URL}/api/trpc`,
 
         // You can pass any HTTP headers you wish here
         // async headers() {
