7e6cdec64d1701a81166fc074162fc3d4877ec76
Carla Bordo <carlapbg@gmail.com>
2024-11-20 17:07:11 -0500

    Style Login Page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -18,15 +18,14 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center"></div>
-      <h1 className="mb-8 text-4xl  font-bold"> SDA Planckster </h1>
-      <h5 className="10s mb-8 animate-pulse text-sm  font-bold">alpha</h5>
-      <div className="w-10"></div>
-      <div className="animate-fade-in mt-8 transform rounded-md border border-gray-300 p-6 shadow-md transition-all duration-300 hover:scale-105 hover:shadow-lg">
+    <div className="flex w-full items-center justify-center">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+      <h1 className="mb-8 text-4xl font-bold animate-pulseGrow"> SkyPulse </h1>
+        <div className="animate-fade-in transform rounded-md border border-gray-300 p-6 shadow-md transition-all duration-300 hover:scale-105 hover:shadow-lg">
         {loggedOut && <div className="mb-4 rounded bg-green-500 p-4 text-white">Successfully logged out.</div>}
         <h2 className="mb-4 text-2xl font-bold">Login</h2>
         <LoginCard buttonAction={handleSubmit} />
+        </div>
       </div>
     </div>
   );
