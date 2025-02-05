aacde979687e688f84424cdd3654b9d4db6c0629
MytsV <mytsvictoria@gmail.com>
2025-02-05 21:09:22 +0200

    Implement swiss grid case study

--- src/lib/infrastructure/server/utils/sda-case-study-utils.ts
+++ src/lib/infrastructure/server/utils/sda-case-study-utils.ts
@@ -1,5 +1,14 @@
 import { z } from "zod";
-import { ClimateKeyframeSchema, ClimateMetadataSchema, ClimateRowSchema, ImageSchema, SentinelKeyframeSchema, SentinelMetadataSchema, SentinelRowSchema } from "~/lib/core/entity/case-study-models";
+import {
+  ClimateKeyframeSchema,
+  ClimateMetadataSchema,
+  ClimateRowSchema,
+  ImageSchema,
+  SentinelKeyframeSchema,
+  SentinelMetadataSchema,
+  SentinelRowSchema, SwissGridKeyframeSchema, SwissGridMetadataSchema,
+  SwissGridRowSchema
+} from "~/lib/core/entity/case-study-models";
 
 /**
  * common
@@ -9,15 +18,16 @@ export const generateMetadataRelativePath = (caseStudyName: string, tracerID: st
   return `${caseStudyName}/${tracerID}/${jobID}/metadata.json`;
 };
 
-export const RegisteredSDACaseStudyNameSchema = z.union([z.literal("climate-monitoring"), z.literal("sentinel-5p")]);
+export const RegisteredSDACaseStudyNameSchema = z.union([z.literal("climate-monitoring"), z.literal("sentinel-5p"), z.literal("swiss-grid")]);
 export type TRegisteredSDACaseStudyName = z.infer<typeof RegisteredSDACaseStudyNameSchema>;
 
-export type TRowSchema = typeof ClimateRowSchema | typeof SentinelRowSchema;
+export type TRowSchema = typeof ClimateRowSchema | typeof SentinelRowSchema | typeof SwissGridRowSchema;
 
 // TODO: additional row schemas
 export const caseStudyToSchema: Record<TRegisteredSDACaseStudyName, TRowSchema> = {
   "climate-monitoring": ClimateRowSchema,
   "sentinel-5p": SentinelRowSchema,
+  "swiss-grid": SwissGridRowSchema,
 };
 
 export const SDAImageSchema = ImageSchema.omit({
@@ -57,11 +67,27 @@ const SDASentinelMetadataSchema = SentinelMetadataSchema.omit({
 });
 
 /**
+ * swiss-grid
+ */
+
+const SDASwissGridKeyframeSchema = SwissGridKeyframeSchema.omit({ images: true }).extend({
+  images: z.array(SDAImageSchema),
+});
+
+const SDASwissGridMetadataSchema = SwissGridMetadataSchema.omit({
+  keyframes: true,
+  expirationTime: true,
+}).extend({
+  keyframes: z.array(SDASwissGridKeyframeSchema),
+  relativePathsForAgent: z.array(z.string()),
+});
+
+/**
  * types
  */
 
-export const SDAKeyframeSchema = z.union([SDAClimateKeyframeSchema, SDASentinelKeyframeSchema]);
+export const SDAKeyframeSchema = z.union([SDAClimateKeyframeSchema, SDASentinelKeyframeSchema, SDASwissGridKeyframeSchema]);
 export type TSDAKeyframe = z.infer<typeof SDAKeyframeSchema>;
 
-export const SDACaseStudyMetadataSchema = z.discriminatedUnion("caseStudy", [SDAClimateMetadataSchema, SDASentinelMetadataSchema]);
+export const SDACaseStudyMetadataSchema = z.discriminatedUnion("caseStudy", [SDAClimateMetadataSchema, SDASentinelMetadataSchema, SDASwissGridMetadataSchema]);
 export type TSDACaseStudyMetadata = z.infer<typeof SDACaseStudyMetadataSchema>;
